b40ac5b526b18ca94ec3294362b39f416763d842
Chir <chirpatelss@gmail.com>
2021-06-09 01:05:40 +0530

    .

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -58,6 +58,9 @@ function Header() {
                             <Route path="/login">
                                 <Login />
                             </Route>
+                            <Route path="/login">
+                                <Login />
+                            </Route>
                             <Route path="/">
                                 <Home/>
                             </Route>
