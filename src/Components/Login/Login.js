3174158fcf6aa16c3d807b9443072640cf47f28d
Chir <chirpatelss@gmail.com>
2021-06-25 23:42:18 +0530

    .

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,5 @@
 import {React,useState,useEffect} from 'react'
+import Loading from '../Loading/Loading'
 import postApi from '../API/postApi';
 import getApi from '../API/getApi'
 import './Login.css';
@@ -7,17 +8,30 @@ const url = process.env.REACT_APP_VERCEL_URL;
 const glitchurl = process.env.REACT_APP_GLITCH_URL;
 function Login() {
     const [selectedFile, setSelectedFile] = useState({});
+    const [awakeLoading, setAwakeLoading] = useState(true);
+    const [tableLoading, setTableLoading] = useState(true);
+    const [uploadLoading, setUploadLoading] = useState(false);
+    const [productSucess, setProductSucess] = useState(false);
     const [name, setName] = useState("");
     const [categories, setCategories] = useState("");
     const [price, setPrice] = useState(0);
     const [universalData, setUniversalData] = useState([]);
     const [reloadTable, setReloadTable] = useState(true);
 
+    useEffect(()=>{
+        const awake= async()=>{
+            await getApi(glitchurl);
+            setAwakeLoading(false);
+        }
+        awake();
+    },[])
+
     useEffect(() => {
         const pageData = async (page, pageSize) => {
             return await getApi(`${url}/data?page=${page}`);
         }
         const dataPageCall = async (totalPage) => {
+            
             let page = 2;
             let temp = [];
             while (page <= totalPage) {
@@ -27,26 +41,33 @@ function Login() {
             setUniversalData((prevState) => { console.log(prevState); return [...prevState, ...temp] })
         }
         const dataCall = async () => {
+            setTableLoading(true)
             let temp = await getApi(url+"/data");
             console.log(temp);
             setUniversalData(temp.data)
             if (temp.page < temp.totalPage) {
                 dataPageCall(temp.totalPage);
             }
+            setTableLoading(false)
         }
         dataCall();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [reloadTable])
     const onNameChange = event => {
+        setProductSucess(false);
         setName(event.target.value)
     }
     const onCategoriesChange = event => {
+        setProductSucess(false);
         setCategories(event.target.value);
     }
     const onPriceChange = event => {
+        setProductSucess(false);
         setPrice(event.target.value);
     }
     const onFileChange = event => {
+        setProductSucess(false);
+        console.log(event.target.files[0])
         setSelectedFile(event.target.files[0]);
     };
     const onFileUpload = async () => {
@@ -65,65 +86,98 @@ function Login() {
             },
             data: formData
         };
-        console.log(await postApi(config))
+        setSelectedFile({});
+        setName("");
+        setCategories("");
+        setPrice(0);
+        document.getElementById("Image").value="";
+        setUploadLoading(true)
+        setProductSucess(false);
+        await postApi(config)
+        setUploadLoading(false)
+        setProductSucess(true);
         setReloadTable(!reloadTable);
     };
     const removeProd = async (id) => {
-        console.log(await getApi(glitchurl+"/imageRemove?id=" + id));
+        await getApi(glitchurl+"/imageRemove?id=" + id);
         setReloadTable(!reloadTable);
     }
     const ignoreList = ["images"]
     return (
         <>
-            <div className="upload">
-                <div className="upload-div">
-                    <h3>Add Product</h3>
-                    <div className="upload-container">
-                        <label for="name">Name</label><br/>
-                        <input type="text" id="name" value={name} onChange={onNameChange} /><br />
-                        <label for="Category" >Category</label><br/>
-                        <input type="text"id="Category"  value={categories} onChange={onCategoriesChange} /><br />
-                        <label for="Price">Price</label><br/>
-                        <input type="text" id="Price" value={price} onChange={onPriceChange} /><br />
-                        <label for="Image">Choose Image</label>
-                        <input type="file" id ="Image"onChange={onFileChange} /><br />
-                        <button onClick={onFileUpload}>Upload!</button>
-                    </div>
+            {awakeLoading &&
+                <div style={{ width: "100%", textAlign: "center",margin:"10px" }}>
+                    <Loading />
                 </div>
-            </div>
-            <div className="products">
-                {universalData.length > 0 &&
-                    <table>
-                        <thead>
-                            <tr>
-                                {// eslint-disable-next-line array-callback-return
-                                    Object.keys(universalData[0]).map((col, index) => {
-                                        if (!ignoreList.includes(col))
-                                            return <th key={index}>{col}</th>
-                                    })}
-                                    <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                universalData.map((data, index) => {
-
-                                    return <tr key={index}>
-
+            }
+            {!awakeLoading &&
+                <>
+                    <div className="upload">
+                        <div className="upload-div">
+                            <h3>Add Product</h3>
+                            <div className="upload-container">
+                                <label for="name">Name</label><br />
+                                <input type="text" id="name" value={name} onChange={onNameChange} /><br />
+                                <label for="Category" >Category</label><br />
+                                <input type="text" id="Category" value={categories} onChange={onCategoriesChange} /><br />
+                                <label for="Price">Price</label><br />
+                                <input type="text" id="Price" value={price} onChange={onPriceChange} /><br />
+                                <label for="Image">Choose Image</label>
+                                <input type="file" id="Image" onChange={onFileChange} /><br />
+                                <button onClick={onFileUpload} className={uploadLoading?"disabled":""}>Upload!</button>
+                                {uploadLoading &&
+                                    <div style={{ width: "100%", textAlign: "center",margin:"10px" }}>
+                                        <Loading width={16} height={16}/>
+                                    </div>
+                                }
+                                {productSucess && !uploadLoading &&
+                                    <div style={{ width: "100%", textAlign: "center",margin:"10px" }}>
+                                        Product added Successfully.
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                    <div className="products">
+                                {tableLoading &&
+                            <div style={{ width: "100%", textAlign: "center",margin:"10px" }}>
+                                <Loading width={32} height={32}/>
+                            </div>
+                        }
+                        {!tableLoading && universalData.length > 0 &&
+                            <table>
+                                <thead>
+                                    <tr>
                                         {// eslint-disable-next-line array-callback-return
-                                            Object.keys(data).map((key, i) => {
-                                                if (!ignoreList.includes(key))
-                                                    return <td key={i}>{data[key]}</td>
+                                            Object.keys(universalData[0]).map((col, index) => {
+                                                if (!ignoreList.includes(col))
+                                                    return <th key={index}>{col.toUpperCase()}</th>
                                             })}
-                                        <td><button onClick={() => { removeProd(data.prodId); }}>Remove</button></td>
+                                        <th>Remove</th>
                                     </tr>
-                                })
-                            }
-                        </tbody>
+                                </thead>
+                                <tbody>
+                                    {
+                                        universalData.map((data, index) => {
 
-                    </table>
-                }
-            </div>
+                                            return <tr key={index}>
+
+                                                {// eslint-disable-next-line array-callback-return
+                                                    Object.keys(data).map((key, i) => {
+                                                        if (!ignoreList.includes(key))
+                                                            return <td key={i}>{data[key]}</td>
+                                                    })}
+                                                <td><button onClick={() => { removeProd(data.prodId); }}>Remove</button></td>
+                                            </tr>
+                                        })
+                                    }
+                                </tbody>
+
+                            </table>
+                        }
+                    </div>
+                </>
+            }
         </>
     )
 }
